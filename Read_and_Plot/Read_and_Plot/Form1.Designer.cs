6662b5678ddb09a1b6c41dd053d7a3f5681c846b
Terry_Goose <guseva.k93@gmail.com>
2017-03-04 22:10:34 +0300

    Read and plot
    Signed-off-by: Terry_Goose <guseva.k93@gmail.com>

---
+++ Read_and_Plot/Read_and_Plot/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Read_and_Plot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button1 = new System.Windows.Forms.Button();
+            this.TestList = new System.Windows.Forms.ComboBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 321);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 33);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Построить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // TestList
+            // 
+            this.TestList.FormattingEnabled = true;
+            this.TestList.Items.AddRange(new object[] {
+            "Test 0: n = [1;10001]   k = n/2",
+            "Test 1: n = [1;10001]",
+            "Test 2: n = 10003         k = [1;10001]",
+            "Test 3: r = 0,001           n = [1;10001]",
+            "Test 4: r = [0,0001;0,01]    n = 10000"});
+            this.TestList.Location = new System.Drawing.Point(12, 242);
+            this.TestList.Name = "TestList";
+            this.TestList.Size = new System.Drawing.Size(132, 21);
+            this.TestList.TabIndex = 2;
+            this.TestList.Tag = "";
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(163, 12);
+            this.chart1.Name = "chart1";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            series4.BorderWidth = 2;
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series4.Color = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            series4.Legend = "Legend1";
+            series4.Name = "Series2";
+            this.chart1.Series.Add(series3);
+            this.chart1.Series.Add(series4);
+            this.chart1.Size = new System.Drawing.Size(468, 342);
+            this.chart1.TabIndex = 3;
+            this.chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(643, 366);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.TestList);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox TestList;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ Read_and_Plot/Read_and_Plot/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Read_and_Plot
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            chart1.Series[0].Points.Clear();
+            chart1.Series[1].Points.Clear();
+            int choice = TestList.SelectedIndex;
+            string fileName = "";
+
+            switch (choice)
+            {
+                case 0:
+                    fileName = "C:\\Users\\кей\\Documents\\Visual Studio 2012\\Projects\\IntersectionSegments\\IntersectionSegments\\bin\\Debug\\Test 0.txt";
+                    break;
+                case 1:
+                    fileName = "C:\\Users\\кей\\Documents\\Visual Studio 2012\\Projects\\IntersectionSegments\\IntersectionSegments\\bin\\Debug\\Test 1.txt";
+                    break;
+                case 2:
+                    fileName = "C:\\Users\\кей\\Documents\\Visual Studio 2012\\Projects\\IntersectionSegments\\IntersectionSegments\\bin\\Debug\\Test 2.txt";
+                    break;
+                case 3:
+                    fileName = "C:\\Users\\кей\\Documents\\Visual Studio 2012\\Projects\\IntersectionSegments\\IntersectionSegments\\bin\\Debug\\Test 3.txt";
+                    break;
+                case 4:
+                    fileName = "C:\\Users\\кей\\Documents\\Visual Studio 2012\\Projects\\IntersectionSegments\\IntersectionSegments\\bin\\Debug\\Test 4.txt";
+                    break;
+            }
+
+            int counter = 0;
+            string line;
+            string[] substrings;
+            
+            System.IO.StreamReader file = new System.IO.StreamReader(fileName);
+            while ((line = file.ReadLine()) != null)
+            {
+                substrings = line.Split(' ');
+                chart1.Series[0].Points.AddXY(double.Parse(substrings[0]), double.Parse(substrings[1]));
+                chart1.Series[1].Points.AddXY(double.Parse(substrings[0]), double.Parse(substrings[2]));
+                counter++;
+            }
+
+            file.Close();
+            chart1.Series[0].LegendText = "Naiv";
+            chart1.Series[1].LegendText = "Effectiv";
+            chart1.DataBind();
+
+        }
+    }
+}
